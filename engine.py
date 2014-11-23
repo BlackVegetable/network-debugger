97397c7e6b98e6f953699bca50e633097d5f6e83
Hadeel Maryoosh <Mila@Hadeels-MacBook-Pro.local>
2014-11-22 19:32:51 -0700

    Edit code and Added most of the side effects needed

--- engine.py
+++ engine.py
@@ -1,4 +1,6 @@
 
+from datetime import datetime
+import traceback
 
 class Engine:    
     def __init__(self):
@@ -6,7 +8,7 @@ class Engine:
         self.arguments = []    
         self.next_function = None # Need an initial function here TODO
     
-    def handle_packet(self, packet,time_elipase=0):
+    def handle_packet(self, pkt,time_elipase=0):
         matched, next_function_and_args, of_rules_to_send = \
                                         self.next_function(packet,
                                                            time_elipase,
@@ -17,7 +19,7 @@ class Engine:
         else:
             self.arguments = [] 
         if matched:
-            self.stacktrace.append([self.next_function.__name__, packet])
+            self.stacktrace.append([self.next_function.__name__, pkt])
         return of_rules_to_send
         
     def get_initial_rules(self):
@@ -25,11 +27,33 @@ class Engine:
 
 # Side effects:
 
+
+# print time and date to console 
+
+print datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
 def __print(text):
     print "Engine (print): " + text
         
-# with open(file_path, "w") as logfile:
-#     Do some stuff, yeah!
-#     logfile.write("Some text")
+with open(file_path, "w") as logfile:
+ #    Do some stuff, yeah!
+  #  logfile.write("Some text")
 
 
+# print the current packet
+def __print(packet):
+    print " Packet: " + str(pkt) 
+
+
+# print OF rules
+#TODO when finish with OF module
+
+
+#traceback
+def another_function():
+    stack()
+
+def stack():
+    traceback.print_stack()
+    print repr(traceback.extract_stack())
+    print repr(traceback.format_stack())
