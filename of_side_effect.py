8c16fcaf532d279e07c801027d6a5906a5cb9515
Devin Ekins <devinjekins@gmail.com>
2014-11-24 10:04:33 -0700

    Added OFSideEffect class/object.
    Should be used by the OF Module as well as the DSM to exchange
    information about OF rules to use.

---
+++ of_side_effect.py
@@ -0,0 +1,33 @@
+
+class OFSideEffect:
+    def __init__(self, command, src_ip=None, dst_ip=None, src_port=None, dst_port=None):
+        if (not command == "add") and (not command == "remove"):
+            raise Exception("command must be 'add' or 'remove'")
+        if (not src_ip) and (not dst_ip) and (not src_port) and (not dst_port):
+            raise Exception("At least one of src_ip, dst_ip, src_port, or dst_port" +
+                            "must be set")
+        self.source_ip = src_ip
+        self.destination_ip = dst_ip
+        self.source_port = src_port
+        self.destination_port = dst_port
+        self.command = command
+
+    def is_opposite(self, other):
+        return (self.source_ip == other.source_ip and
+                self.destination_ip == other.destination_ip and
+                self.source_port == other.source_port and
+                self.destination_port == other.destination_port and
+                ((self.command == "add" and other.command == "remove") or
+                 (self.command == "remove" and other.command == "add"))
+
+    def __str__(self):
+        s = self.command + ":"
+        if self.source_ip:
+            s += " source ip = " + str(self.source_ip)
+        if self.destination_ip:
+            s += " destination ip = " + str(self.destination_ip)
+        if self.source_port:
+            s += " source port = " + str(self.source_port)
+        if self.destination_port:
+            s += " destination port = " + str(self.destination_port)
+        return s
