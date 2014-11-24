9567f866acd47498a4e7ce1cff960323297f7edf
icfy <fy.icfy@gmail.com>
2014-11-23 23:07:41 -0700

    fixed a heartbreaking bug...
    typt ->type

--- dsml_controller.py
+++ dsml_controller.py
@@ -12,27 +12,48 @@ class dsml_controller (object):
     def __init__ (self, connection):
         self.connection = connection
 
-        connection.addListeners(self)
-        core.openflow.clear_flows_of_connect = 1
+        self.connection.addListeners(self)
         
+        core.openflow.clear_flows_of_connect = 1
 
     # Handles packet in messages from the switch.
     def _handle_PacketIn (self, event):
   
         packet = event.parsed
-        print packet
+        print packet.type
     
-        match = of.ofp_match.from_packet(packet)       
+        #match = of.ofp_match.from_packet(packet)       
         msg = of.ofp_flow_mod()
-        
+                       
         msg.match.nw_dst = "10.1.1.2"
-        msg.match.nw_src = "10.1.1.5"        
-        msg.match.dl_typt = pkt.ethernet.IP_TYPE
-            
+        msg.match.nw_src = "10.1.1.5"      
+        msg.match.dl_type = 0x800
+        msg.match.nw_proto = 1
+        msg.idle_timeout = 20
+        msg.hard_timeout = 20
+        msg.actions = []
         msg.actions.append(of.ofp_action_output(port = of.OFPP_NORMAL))
+        msg.actions.append(of.ofp_action_output(port = 6633))
         msg.actions.append(of.ofp_action_output(port = 1337))
 
         self.connection.send(msg)
+        
+        msg = of.ofp_flow_mod()
+
+        msg.match.nw_dst = "10.1.1.5"
+        msg.match.nw_src = "10.1.1.2"      
+        msg.match.dl_type = 0x800
+        msg.match.nw_proto = 1
+        msg.idle_timeout = 20
+        msg.hard_timeout = 20
+        msg.actions = []
+        msg.actions.append(of.ofp_action_output(port = of.OFPP_NORMAL))
+        msg.actions.append(of.ofp_action_output(port = 6633))
+        msg.actions.append(of.ofp_action_output(port = 1337))
+
+        self.connection.send(msg)
+        
+        
         
         print "handle packetIn do nothing, just printing"
 
