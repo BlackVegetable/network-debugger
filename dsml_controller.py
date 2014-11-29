0b50b7e844fd9ab34dc198ac05c86bfbbbf5bec3
icfy <fy.icfy@gmail.com>
2014-11-29 16:20:27 -0700

    tested add and delete rules

--- dsml_controller.py
+++ dsml_controller.py
@@ -1,4 +1,3 @@
-
 # This is a test controller for flow rules test. the dst, src, dl_type, dw_proto and out port are all hard coded.  
 
 from pox.core import core
@@ -7,65 +6,44 @@ import pox.lib.packet as pkt
 from pox.lib.addresses import IPAddr
 import pox.misc.arp_responder as arp_responder
 #from engine import *
-
+import time
+from pox.entry_operation import *
 log = core.getLogger()
 
-class dsml_controller (object):
+class delete_controller (object):
     def __init__ (self, connection):
         self.connection = connection
 
         self.connection.addListeners(self)
         
         core.openflow.clear_flows_of_connect = 1
+        
+        #add_entry(connection, nw_dst, nw_src, dl_type = 0x800, nw_proto = 1)
+        add_entry(self.connection, nw_dst="10.1.1.2", nw_src="10.1.1.5", nw_proto=1)
+        add_entry(self.connection, nw_dst="10.1.1.2", nw_src="10.1.1.5", nw_proto=6)
+        add_entry(self.connection, nw_dst="10.1.1.5", nw_src="10.1.1.2", nw_proto=1)
+        add_entry(self.connection, nw_dst="10.1.1.5", nw_src="10.1.1.2", nw_proto=6)
+        
+        #raw_input("\n\nPress enter key to exit.")
+        time.sleep(5)
+        
+        delete_entry(self.connection, nw_dst="10.1.1.2", nw_src="10.1.1.5", nw_proto=1)
+        delete_entry(self.connection, nw_dst="10.1.1.2", nw_src="10.1.1.5", nw_proto=6)
 
+        
     # Handles packet in messages from the switch.
     def _handle_PacketIn (self, event):
-  
         packet = event.parsed
-        print packet.type
-    
-        match = of.ofp_match.from_packet(packet)       
-        print match
-        msg = of.ofp_flow_mod()
         
-        msg.match.nw_dst = "10.1.1.2"
-        msg.match.nw_src = "10.1.1.5"      
+        print "incoming packets"
         
-        #IP packet type
-        msg.match.dl_type = 0x800
         
-        #nw_port = {1,6,17} for TCP/UDP/ICMP
-        msg.match.nw_proto = 6
-        msg.actions = []
-        msg.actions.append(of.ofp_action_output(port = of.OFPP_NORMAL))
         
-        #adding this port seems can once agin send packet back to the controller
-        #msg.actions.append(of.ofp_action_output(port = 6633))
         
-        #port for listen
-        msg.actions.append(of.ofp_action_output(port = 1337))
-
-        self.connection.send(msg)
-        
-        msg = of.ofp_flow_mod()
-
-        msg.match.nw_dst = "10.1.1.5"
-        msg.match.nw_src = "10.1.1.2"      
-        msg.match.dl_type = 0x800
-        msg.match.nw_proto = 6
-        msg.actions = []
-        msg.actions.append(of.ofp_action_output(port = of.OFPP_NORMAL))
-        #msg.actions.append(of.ofp_action_output(port = 6633))
-        msg.actions.append(of.ofp_action_output(port = 1337))
-
-        self.connection.send(msg)
-
-        print "Rules set 10.1.1.5 <--> 10.1.1.2"
-
 def launch ():
     arp_responder.launch(no_learn = True)
     def start_switch (event):
-        dsml_controller(event.connection)
+        delete_controller(event.connection)
+        
 
     core.openflow.addListenerByName("ConnectionUp", start_switch)
-
---
+++ entry_operation.py
@@ -0,0 +1,26 @@
+from pox.core import core
+import pox.openflow.libopenflow_01 as of
+import pox.lib.packet as pkt
+from pox.lib.addresses import IPAddr
+
+
+def delete_entry(connection, nw_dst, nw_src, dl_type = 0x800, nw_proto = 1):
+    msg = of.ofp_flow_mod()
+    msg.match.nw_dst = nw_dst
+    msg.match.nw_src = nw_src      
+    msg.match.dl_type = dl_type
+    msg.match.nw_proto = nw_proto
+    msg.command =of.OFPFC_DELETE
+    #print msg
+    connection.send(msg)
+    
+def add_entry(connection, nw_dst, nw_src, dl_type = 0x800, nw_proto = 1):
+    msg = of.ofp_flow_mod()
+    msg.match.nw_dst = nw_dst
+    msg.match.nw_src = nw_src      
+    msg.match.dl_type = dl_type
+    msg.match.nw_proto = nw_proto
+    msg.actions = []
+    msg.actions.append(of.ofp_action_output(port = of.OFPP_NORMAL))
+
+    connection.send(msg)
