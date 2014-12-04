a1e16ddc3e427374420817de50fc2e28522bb8be
Devin Ekins <devinjekins@gmail.com>
2014-12-04 16:51:17 -0700

    Removed sniffer, moved functionality.
    The sniffer is no longer needed. A single function in the
    controller should be able to replace its ability to convert
    packets into scapy-friendly packets.
    
    The controller has a TODO section marked that needs to be
    completed ASAP. We need the POX-style function that deals with
    packets that arrive on its interface. This function's code has
    been commented out but is mostly complete. The remaining step
    is to write cleanup code for when the debugger needs to exit.

--- dsml_controller.py
+++ dsml_controller.py
@@ -1,23 +1,15 @@
-# This is a test controller for flow rules test. the dst, src, dl_type, dw_proto and out port are all hard coded.  
 
 from pox.core import core
 import pox.openflow.libopenflow_01 as of
 import pox.lib.packet as pkt
 from pox.lib.addresses import IPAddr
 import pox.misc.arp_responder as arp_responder
-
 from pox.entry_operation import *
 
-
-import thread
-from dsml_sniffer import *
-
 from scapy.all import *
-
+import sys
 import test_dsm as dsm
-
 import time
-log = core.getLogger()
 
 class dsml_controller (object):
 
@@ -57,15 +49,25 @@ class dsml_controller (object):
         #add_entry(self.connection, nw_dst="10.1.1.2", nw_src="10.1.1.5", nw_proto=6)
         
         self.start_sniffer ("127.0.0.1", "1337", "tcp")
-        #try:
-        #    thread.start_new_thread( self.start_sniffer, ("127.0.0.1", "1337", "tcp",) )
-        #except Exception as e:
-        #    print type(e).__name__
-        #    print "Error: unable to start sniffer"
+        
+    def convert_to_scapy_packet(self, pox_packet):
+        '''Convert a POX-style packet to a Scapy-style
+        packet for use in the DSM.'''
+        raw_bytes = pox_packet.raw
+        return Ether(raw_bytes)
+
+    # TODO: Replace this Pseudo-code
+    # --- Function to receive packets ---
+    # scapy_packet = self.convert_to_scapy_packet(pox_packet)
+    # return_list = self.engine.handle_packet(scapy_packet)
+    # if return_list[0] == True:
+    #     if len(return_list) > 1:
+    #         self.write_entry(return_list[1:])
+    #     pass # Later, we will reset the timer.
+    # elif return_list[0] == "Exit":
+    #     Cleanup resources gracefully.
+    #     Will probably finish by calling sys.exit(0) 
     
-    def start_sniffer(self, IP, port, protocol):
-        sniffer = Dsml_sniffer(self)
-        sniffer.start_sniffing(IP, port, protocol)
     def write_entry(self, rules):
         for item in rules:
             if item.command == "add":
@@ -79,21 +81,9 @@ class dsml_controller (object):
                     tp_src = None
                 add_sniff(self.connection, nw_dst=item.destination_ip, nw_src=item.source_ip, tp_dst=tp_dst, tp_src=tp_src)
     
-    # sniff and change rules of switch
-    #def rules_changing (self, connection):
-    #    print "test if we can go to this stage"
-    #    rule = []
-    #    a = sniff(filter="tcp and (port 1337)",count =1)
-    #    print a
-    #    while (a)!=0:
-    #        print a
-    #        rule = self.engine.handle_packet(a)
-    #        print rule
-    #        a = sniff(filter="tcp and (port 1337)",count =1)
 def launch ():
     arp_responder.launch(no_learn = True)
     def start_switch (event):
         dsml_controller(event.connection)
         
-
     core.openflow.addListenerByName("ConnectionUp", start_switch)
